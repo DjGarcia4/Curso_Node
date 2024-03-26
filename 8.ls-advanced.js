34f338bd4c7c7e756a9172d32bcd9261acd289ad
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-03-26 10:00:49 -0600

    ls-advandced 2.0

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,13 +1,38 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
+const path = require("node:path");
 
 const folder = process.argv[2] ?? ".";
 
-fs.readdir(folder, (err, files) => {
-  if (err) {
-    console.log("Fallo  al leer el directorio", err);
-    return;
+async function ls(folder) {
+  let files;
+  try {
+    files = await fs.readdir(folder);
+  } catch (error) {
+    console.error(`No se puede leer el directorio ${folder}`);
+    process.exit(1);
   }
-  files.forEach((file) => {
-    console.log(file);
+  const filesPromises = files.map(async (file) => {
+    const filePath = path.join(folder, file);
+    let stats;
+
+    try {
+      stats = await fs.stat(filePath);
+    } catch (error) {
+      console.log(`No se pudo leer el archivo ${filePath}`);
+      process.exit(1);
+    }
+    const isDirectory = stats.isDirectory();
+    const fileType = isDirectory ? "d" : "f";
+    const fileSize = stats.size;
+    const fileModified = stats.mtime.toLocaleString();
+
+    return `${fileType} ${file.padEnd(20)} ${fileSize
+      .toString()
+      .padStart(10)} ${fileModified}`;
   });
-});
+
+  const filesInfo = await Promise.all(filesPromises);
+  filesInfo.forEach((fileInfo) => console.log(fileInfo));
+}
+
+ls(folder);
