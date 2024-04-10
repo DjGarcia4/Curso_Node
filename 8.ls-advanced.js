880a43aa96ac30ee4970322f1e055831a04a8196
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-04-09 20:59:32 -0600

    Instalando dependencias

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,5 +1,6 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
+const pc = require("picocolors");
 
 const folder = process.argv[2] ?? ".";
 
@@ -8,7 +9,7 @@ async function ls(folder) {
   try {
     files = await fs.readdir(folder);
   } catch (error) {
-    console.error(`No se puede leer el directorio ${folder}`);
+    console.error(pc.red(`No se puede leer el directorio ${folder}`));
     process.exit(1);
   }
   const filesPromises = files.map(async (file) => {
@@ -26,9 +27,10 @@ async function ls(folder) {
     const fileSize = stats.size;
     const fileModified = stats.mtime.toLocaleString();
 
-    return `${fileType} ${file.padEnd(20)} ${fileSize
+    return `${fileType} ${pc.blue(file.padEnd(20))} ${pc
+      .green(fileSize)
       .toString()
-      .padStart(10)} ${fileModified}`;
+      .padStart(10)} ${pc.yellow(fileModified)}`;
   });
 
   const filesInfo = await Promise.all(filesPromises);
